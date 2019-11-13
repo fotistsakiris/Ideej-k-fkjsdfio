cc43569f96488a820a102aa7e887f63068726c40
Fotis Tsakiris <footios76@gmail.com>
2019-11-13 10:48:01 +0200

    fixed bug with autologout

--- screens/StartUpScreen.js
+++ screens/StartUpScreen.js
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 
 import * as authActions from '../store/actions/auth';
 import Colours from '../constants/Colours';
+import CustomLinearGradient from '../components/UI/CustomLinearGradient';
 
 const StartUpScreen = (props) => {
 	const dispatch = useDispatch();
@@ -14,22 +15,23 @@ const StartUpScreen = (props) => {
 			const tryLogin = async () => {
 				// Note: getItem is asynchronous, so we get a promise
 				const userData = await AsyncStorage.getItem('userData');
+				
 				if (!userData) {
 					props.navigation.navigate('Auth');
 					return;
 				}
+				// parse converts a string to an object or array
 				const transformedData = JSON.parse(userData);
 				const { token, userId, expiryDate } = transformedData;
 				const expirationDate = new Date(expiryDate); // expiryDate is string in ISO format...
-				
+
 				if (expirationDate <= new Date() || !token || !userId) {
 					props.navigation.navigate('Auth');
 					return;
 				}
-				
-				
-				const expirationTime = expirationDate.getTime - new Date().getTime
-				props.navigation.navigate('Shop');
+
+				const expirationTime = expirationDate.getTime() - new Date().getTime();
+				props.navigation.navigate('Main');
 				dispatch(authActions.authenticate(token, userId, expirationTime));
 			};
 
@@ -39,9 +41,11 @@ const StartUpScreen = (props) => {
 	);
 
 	return (
-		<View style={styles.centered}>
-			<ActivityIndicator size="large" color={Colours.chocolate} />
-		</View>
+		<CustomLinearGradient>
+			<View style={styles.centered}>
+				<ActivityIndicator size="large" color={Colours.moccasin_light} />
+			</View>
+		</CustomLinearGradient>
 	);
 };
 
